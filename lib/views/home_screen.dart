a0cc35a82980bd626a22f9c36b1289e4cb20a0cc
Jaimul Islam Akik <105303801+devjaimul@users.noreply.github.com>
2024-07-15 23:31:24 +0600

    updated

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,35 +1,53 @@
-import 'package:bdtask_assignmnet/utils/text_style.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white.withOpacity(0.8),
-      appBar: AppBar(
-        title: const CustomTextStyle(data: 'Bio Data',color: Colors.white,fontSize: 25,),
-        centerTitle: true,
-       backgroundColor: Colors.blueGrey,
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomTextStyle(data: 'Name : Jaimul Islam Akik'),
-            CustomTextStyle(data: 'Roll : 550675'),
-            CustomTextStyle(data: 'Address : Faridpur, Bangladesh'),
-            CustomTextStyle(data: 'College : Faridpur Polytechnic Institute'),
-            CustomTextStyle(data: 'Profession : Flutter Developer'),
-
-
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
       ),
     );
   }
 }
-
-
